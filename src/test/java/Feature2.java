63089c1e9e1d6c783bcd34b0eed002ba0af0b38f
Nese-yigit <72082269+Nese-yigit@users.noreply.github.com>
2020-10-14 20:51:56 +0200

    added changes from github
    changed

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -2,6 +2,6 @@ public class Feature2 {
 
 
     public static void main(String[] args) {
-        System.out.println("working on feature 2");
+        System.out.println("working on feature 2 i cahnged something");
     }
 }
